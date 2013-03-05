9143fbe8704a9f419c99f6e667479151127baa34
Maximilian Schoening <mschoening@me.com>
2013-03-05 15:31:34 -0800

    Set accept header

--- init.rb
+++ init.rb
@@ -9,10 +9,9 @@ class Heroku::Command::Lunch < Heroku::Command::Base
   # output this weeks's lunches
   #
   def index
-    meals = json_decode(RestClient::Resource.new('http://lunch.herokuapp.com/').get.body)
+    meals = json_decode(RestClient::Resource.new("http://lunch.herokuapp.com/", :accept => :json).get.body)
 
     meals.each do |meal|
-      # puts "#{ Date.parse(meal['date']) }: #{ meal['summary'] }"
       puts "#{ Date.parse(meal['date']).strftime("%A") }: #{ meal['summary'] }"
     end
   end
